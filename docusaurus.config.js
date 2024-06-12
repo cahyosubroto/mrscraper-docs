356a3a1aaeb401e2a174961df75c1e20d6c8e9d0
pr3b <eb.underwood22@gmail.com>
2024-06-12 12:01:16 +0700

    config docs baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mrscraper-docs.netlify.app',
+  url: 'https://docs.mrscraper.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
