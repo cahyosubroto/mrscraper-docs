a638c61766e6ae3eeae7726abc4523e952e8cea3
pr3b <eb.underwood22@gmail.com>
2024-06-12 10:29:22 +0700

    config docs baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   organizationName: 'cahyosubroto', // Usually your GitHub org/user name.
   projectName: 'mrscraper-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
